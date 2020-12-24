d89a00c94173f3c55d34a50426bf811191700a81
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-24 12:10:03 +0530

    Refactor:Doublet combination of the coin

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,34 +1,40 @@
 #!/bin/bash -x
 echo "Welcome to Flip Coin Combination problem"
-
-declare -A singlet                                        #storing the singlet in Dictionary
-
+declare -A doublet                                         #storing the Doublet combination in Dictionary
 #Variable
 flipCoin=1
-function checkResult ()                                   #fuction for checking random value
+
+function checkResult ()                                    #fuction for checking random value
 {
-     case $1 in                                           #Case for random value
-	0)
-	singlet[head]=$(( ${singlet[head]} + 1 ))
+    case $1$2 in                                           #Case for 2 random values(HH,TT,HT,TH)
+	00)
+	doublet[HH]=$(( ${doublet[HH]} + 1 ))
 	;;
-	1)
-	singlet[tail]=$(( ${singlet[tail]} + 1 ))
+	11)
+	doublet[TT]=$(( ${doublet[TT]} + 1 ))
+	;;
+	01)
+	doublet[HT]=$(( ${doublet[HT]} + 1 ))
+	;;
+	10)
+	doublet[TH]=$(( ${doublet[TH]} + 1 ))
 	;;
 	*)
 	echo "error"
 	;;
-     esac
+    esac
 }
-
-while [ $flipCoin -le 20 ]                                 #for flipping the coin for 20 times
+while [ $flipTheCoin -le 20 ]                               #for flipping the coin for 20 times
 do
-      	random=$(( RANDOM % 2 ))
-	checkResult $random                                #calling checkResult function
+	random1=$(( RANDOM % 2 ))
+	random2=$(( RANDOM % 2 ))
+	checkResult $random1 $random2                       #calling checkResult function
 	flipCoin=$(( $flipCoin + 1 ))
 done
-echo "singlet values in percentage"
-for key in ${!singlet[@]}                                  #for printing the percentage of heads and tails
+echo "Doublet values in percentage"
+for key in ${!doublet[@]}                                   #for printing the percentage of each doublet combination
 do
-	percentage=`echo | awk -v key="$key" -v value="${singlet[$key]}" '{ percent = (value / 50) * 100 } END { print key " is " percent "%" }'`
-        echo "$percentage
+	percentage=`echo | awk -v key="$key" -v value="${doublet[$key]}" '{ percent = (value / 20) * 100 } END { print key " is " percent "%" }'`
+	echo "$percentage"
+
 done
