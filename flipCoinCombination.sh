b1a16bf7f950731885f9e830855bafd203eb7306
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-24 12:14:43 +0530

    Refactor:Triplet combination of the coin

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,40 +1,52 @@
 #!/bin/bash -x
 echo "Welcome to Flip Coin Combination problem"
-declare -A doublet                                         #storing the Doublet combination in Dictionary
-#Variable
+declare -A triplet                                          #storing the Triplet combination in Dictionary
+#variable
 flipCoin=1
 
-function checkResult ()                                    #fuction for checking random value
+function checkResult ()                                     #fuction for checking random value
 {
-    case $1$2 in                                           #Case for 2 random values(HH,TT,HT,TH)
-	00)
-	doublet[HH]=$(( ${doublet[HH]} + 1 ))
-	;;
-	11)
-	doublet[TT]=$(( ${doublet[TT]} + 1 ))
-	;;
-	01)
-	doublet[HT]=$(( ${doublet[HT]} + 1 ))
-	;;
-	10)
-	doublet[TH]=$(( ${doublet[TH]} + 1 ))
-	;;
-	*)
-	echo "error"
-	;;
-    esac
+	case $1$2$3 in                                      #Case for 3 random values (HHH,HHT,HTH,THH,TTH,HTT,THT,TTT)
+		000)
+		triplet[HHH]=$(( ${triplet[HHH]} + 1 ))
+		;;
+		001)
+		triplet[HHT]=$(( ${triplet[HHT]} + 1 ))
+		;;
+		010)
+		triplet[HTH]=$(( ${triplet[HTH]} + 1 ))
+		;;
+		100)
+		triplet[THH]=$(( ${triplet[THH]} + 1 ))
+		;;
+		110)
+		triplet[TTH]=$(( ${triplet[TTH]} + 1 ))
+		;;
+		011)
+		triplet[HTT]=$(( ${triplet[HTT]} + 1 ))
+		;;
+		101)
+		triplet[THT]=$(( ${triplet[THT]} + 1 ))
+		;;
+		111)
+		triplet[TTT]=$(( ${triplet[TTT]} + 1 ))
+		;;
+		*)
+		echo "error"
+		;;
+	esac
 }
-while [ $flipTheCoin -le 20 ]                               #for flipping the coin for 20 times
+while [ $flipCoin -le 50 ]                                      #for flipping the coin for 50 times
 do
 	random1=$(( RANDOM % 2 ))
 	random2=$(( RANDOM % 2 ))
-	checkResult $random1 $random2                       #calling checkResult function
+	random3=$(( RANDOM % 2 ))
+	checkResult $random1 $random2 $random3                  #calling CheckResult for 3 random values
 	flipCoin=$(( $flipCoin + 1 ))
 done
-echo "Doublet values in percentage"
-for key in ${!doublet[@]}                                   #for printing the percentage of each doublet combination
-do
-	percentage=`echo | awk -v key="$key" -v value="${doublet[$key]}" '{ percent = (value / 20) * 100 } END { print key " is " percent "%" }'`
-	echo "$percentage"
 
+for key in ${!triplet[@]}                                       #for printing the percentage of each triplet combination
+do
+	percentage=`echo | awk -v key="$key" -v value="${triplet[$key]}" '{ percent = (value / 50) * 100 } END { print key " is " percent "%" }'`
+	echo "$percentage"
 done
