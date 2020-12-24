033b39538632aef9411fe5f0f88afcf981f57d7e
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-24 12:00:28 +0530

    Add:flipping coin for heads or tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,12 @@
 echo "Welcome to Flip Coin Combination problem"
+
+#!/bin/bash -x
+
+Toss=$((RANDOM%2))                                        #Generating a random value
+
+if [[ $Toss -eq 1 ]]                                      #Checking a random value
+then
+   echo "Heads"
+else
+   echo "Tails"
+fi
